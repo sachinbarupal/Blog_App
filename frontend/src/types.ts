5b5fc15488ec9c204b964cf39909d875f4cadb4c
sachinbarupal <sachinbarupal@gmail.com>
2024-07-12 22:47:53 +0530

    frontend init

---
+++ frontend/src/types.ts
@@ -0,0 +1,13 @@
+export type signinInput = {
+  username: string;
+  password: string;
+};
+export type createBlogInput = {
+  title: string;
+  content: string;
+};
+export type updateBlogInput = {
+  id: number;
+  title?: string;
+  content?: string;
+};
